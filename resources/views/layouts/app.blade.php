9ac7839169d3decf5c29b95849c6d70dd65cfa0d
Jesus Garcia <jesus.garcia.cortes13@gmail.com>
2024-06-05 17:09:07 +0200

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="https://cdn.datatables.net/2.0.7/css/dataTables.dataTables.min.css" rel="stylesheet" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
